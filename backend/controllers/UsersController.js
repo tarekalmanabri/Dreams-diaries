ca0451b041ad585bd18c50648149fcfdf8bc0152
Tarek Almanabri <tarek.almanabri94@gmail.com>
2021-08-09 17:10:41 +0200

    update backend

--- backend/controllers/UsersController.js
+++ backend/controllers/UsersController.js
@@ -1,3 +1,5 @@
+const fetch = require("node-fetch");
+
 const users = [
   {
     id: 1,
@@ -9,7 +11,19 @@ const users = [
   },
 ];
 
+const api_key = process.env.API_KEY;
+
 const UsersController = {
+  async getCompany(req, res) {
+    console.log(api_key);
+    const companies = await fetch(
+      `https://www.themuse.com/api/public/companies?page=1&api_key=${api_key}`
+    );
+    const json = await companies.json();
+
+    res.json(json);
+  },
+
   getUsers(req, res) {
     res.json(users);
   },
--- backend/routes.js
+++ backend/routes.js
@@ -4,6 +4,7 @@ const createRoutes = (app) => {
   app.get("/", (req, res) => {
     res.send({ status: true });
   });
+  app.get("/companies", UsersController.getCompany);
 
   app.get("/users", UsersController.getUsers);
   app.post("/users", UsersController.postUsers);
