ecbbda8eb23de636fb8a6e77e9018ac37b986a81
Tarek Manabri <tarek@Tareks-MacBook-Air.local>
2021-08-10 17:55:33 +0200

    updated styls in Company.vue

--- frontend/src/components/Company.vue
+++ frontend/src/components/Company.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
     <a :href="company.refs.jobs_page">
-      <div class="bg-gray-500 m-2 p-2 flex justify-evenly">
-        <h1>{{ company.name }}</h1>
-        <p>{{ company.description }}</p>
-        <p class="p-2">
-          <strong>{{ company.industries[0].name }}</strong>
-        </p>
-        <img :src="company.refs.logo_image" alt="" class="w-28" />
+      <div class="border-2 rounded m-2 p-2 flex justify-between">
+        <img :src="company.refs.logo_image" alt="" class="w-24" />
+        <h1 class="ml-5 mr-auto text-2xl self-left">{{ company.name }}</h1>
+        <p class="m-1 self-center">{{ company.description }}</p>
+        <div class="flex flex-col justify-end">
+          <p class="m-1">
+            <strong>{{ company.industries[0].name }}</strong>
+          </p>
+        </div>
       </div>
     </a>
   </div>
