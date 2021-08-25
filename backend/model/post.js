21079b3dc2a3694044ee52bb20092d8ee29831ad
Tarek Almanabri <tarek.almanabri94@gmail.com>
2021-08-25 17:16:03 +0200

    created posts schema and controlle

---
+++ backend/model/post.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("post", postSchema);
